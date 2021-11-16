d785c6d93d93d290ffaeaa11f8c13583df78819f
Linnkid_CHEN <56332312+LinnkidChen@users.noreply.github.com>
2021-11-17 01:02:07 +0800

    init

--- Recursive/Rec_snytatic_analysis.cpp
+++ Recursive/Rec_snytatic_analysis.cpp
@@ -1,16 +1,17 @@
 #include <cctype>
 #include <fstream>
 #include <iostream>
+#include <map>
+#include <set>
 #include <string>
 #include <vector>
-
 #define TERM 1
 #define NON_TERM 2
 using namespace std;
 
 struct syb // symbol
 {
-  char ch;
+  int ch;
   int type; // either Term or non_term. Term stands for terminal.
 };
 
@@ -19,32 +20,64 @@ struct prod { // production
   vector<syb> right;
 };
 
-int is_term(char c) {
-  if (isupper(c))
-    return NON_TERM;
-  else
-    return TERM;
+map<string, int> syb_map;
+set<int> Non_Term;
+// int is_term(char c) {
+//   if (isupper(c))
+//     return NON_TERM;
+//   else
+//     return TERM;
+// }
+int map_syb_fun(string ch) {
+
+  if (syb_map.find(ch) == syb_map.end()) // not find
+  {
+    syb_map.insert(map<string, int>::value_type(ch, syb_map.size()));
+  }
+  return syb_map.find(ch)->second;
 }
 void read_grammar(ifstream &fin, vector<prod> &grammar) {
   string temp_s;
   prod temp_p;
-
+  size_t div; // place of "->"
   while (!fin.eof()) {
     syb temp_sym;
     temp_p.right.clear();
+
     fin >> temp_s;
-    temp_sym.ch = temp_s[0];
-    temp_sym.type = is_term(temp_s[0]);
+    div = temp_s.find("->");
+    temp_sym.ch = map_syb_fun(temp_s.substr(0, div));
     temp_p.left = temp_sym;
 
-    for (auto it = temp_s.begin() + 3; it != temp_s.end(); it++) {
-      temp_sym.ch = *it;
-      temp_sym.type = is_term(*it);
+    for (auto it = temp_s.begin() + 2 + div; it != temp_s.end(); it++) {
+      temp_sym.ch = map_syb_fun(string(1, *it));
       temp_p.right.push_back(temp_sym);
     }
     grammar.push_back(temp_p);
   }
+  for (auto it = grammar.begin(); it != grammar.end(); it++) {
+    Non_Term.insert(it->left.ch);
+  }
+  for (auto it = grammar.begin(); it != grammar.end(); it++) {
+    it->left.type = NON_TERM;
+    for (auto it_ = it->right.begin(); it_ != it->right.end(); it_++) {
+      if (Non_Term.find(it_->ch) != Non_Term.end()) { // is a non_terminal
+                                                      // symbol
+        it_->type = NON_TERM;
+      } else
+        it_->type = TERM;
+    }
+  }
 }
+// void rmv_left_rec(vector<prod> &grammar) {
+//   for (auto it = grammar.begin(); it != grammar.end(); it++) {
+//     if (it->left.ch == it->right.begin()->ch) // is left recursive
+//     {
+//       char new_non_prd, left = it->left.ch;
+//       vector<prod> temp_gmr;
+//     }
+//   }
+// }
 void print_grammar(ofstream &fout, vector<prod> const &grammar) {
   for (auto it = grammar.begin(); it != grammar.end(); it++) {
     fout << it->left.ch << "->";
