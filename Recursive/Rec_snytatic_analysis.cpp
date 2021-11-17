3d6049c5758de704306daf3efeed6297db0c9477
Linnkid_CHEN <56332312+LinnkidChen@users.noreply.github.com>
2021-11-17 21:45:45 +0800

    NOV17

--- Recursive/Rec_snytatic_analysis.cpp
+++ Recursive/Rec_snytatic_analysis.cpp
@@ -36,6 +36,15 @@ int map_syb_fun(string ch) {
   }
   return syb_map.find(ch)->second;
 }
+string map_syb_fun(int ch) {
+  string rslt;
+  rslt = "";
+  for (auto it = syb_map.begin(); it != syb_map.end(); it++) {
+    if (it->second == ch)
+      rslt = it->first;
+  }
+  return rslt;
+}
 void read_grammar(ifstream &fin, vector<prod> &grammar) {
   string temp_s;
   prod temp_p;
@@ -68,21 +77,61 @@ void read_grammar(ifstream &fin, vector<prod> &grammar) {
         it_->type = TERM;
     }
   }
+  map_syb_fun("");
 }
-// void rmv_left_rec(vector<prod> &grammar) {
-//   for (auto it = grammar.begin(); it != grammar.end(); it++) {
-//     if (it->left.ch == it->right.begin()->ch) // is left recursive
-//     {
-//       char new_non_prd, left = it->left.ch;
-//       vector<prod> temp_gmr;
-//     }
-//   }
-// }
+void rmv_left_rec(vector<prod> &grammar) {
+  vector<prod> temp_gmr;
+  for (auto it = grammar.begin(); it != grammar.end(); it++) {
+    if (it->left.ch == it->right.begin()->ch) // is left recursive
+    {
+      string new_non_prd;
+      string left = map_syb_fun(it->left.ch);
+      syb new_nonp_syb, temp_s;
+
+      prod temp_p;
+
+      new_non_prd = left + '\'';
+      new_nonp_syb.ch = map_syb_fun(new_non_prd);
+      new_nonp_syb.type = NON_TERM;
+
+      for (auto it_ = grammar.begin();
+           it_ != grammar.end();) {               // for every prod in grammar
+        if (map_syb_fun(it_->left.ch) == left) {  // is left recursive
+          if (it_->left.ch == it_->right[0].ch) { // A->Ab
+            temp_p.left = new_nonp_syb;
+            for (auto it__ = it_->right.begin() + 1; it__ != it_->right.end();
+                 it__++) {
+              temp_p.right.push_back(*it__);
+            }
+            temp_p.right.push_back(new_nonp_syb);
+          }      // add A'->bA'
+          else { // A->b then add A->bA'
+            temp_p.left = it_->left;
+            temp_p.right = it_->right;
+            temp_p.right.push_back(new_nonp_syb);
+          }
+          temp_gmr.push_back(temp_p);
+          temp_p.right.clear();
+          it_ = grammar.erase(it_);
+        } else
+          it_++;
+      }
+      // add A'->none
+      temp_p.left = new_nonp_syb;
+      temp_s.type = TERM;
+      temp_s.ch = map_syb_fun("");
+      temp_gmr.push_back(temp_p);
+    }
+  }
+  for (auto it_ = temp_gmr.begin(); it_ != temp_gmr.end(); it_++) {
+    grammar.push_back(*it_);
+  }
+}
 void print_grammar(ofstream &fout, vector<prod> const &grammar) {
   for (auto it = grammar.begin(); it != grammar.end(); it++) {
-    fout << it->left.ch << "->";
+    fout << map_syb_fun(it->left.ch) << "->";
     for (auto it_ = it->right.begin(); it_ != it->right.end(); it_++) {
-      fout << it_->ch;
+      fout << map_syb_fun(it_->ch);
     }
     fout << endl;
   }
@@ -100,6 +149,7 @@ int main() {
 
   vector<prod> grammar;
   read_grammar(fin, grammar);
+  rmv_left_rec(grammar);
   print_grammar(fout, grammar);
   return 0;
 }
